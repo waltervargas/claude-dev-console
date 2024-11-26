8eb07f8af8948936524d00daf4c8e45385d273bf
Walter Vargas <waltervargas@linux.com>
2024-11-26 21:34:28 +0100

    Add call to claude

--- src/components/response.py
+++ src/components/response.py
@@ -4,6 +4,6 @@ from src.services.claude import call_claude_sonnet_3_5
 def display_response_markdown(system, assistant, prompt, code, additional_file):
     if st.button("Run"):
         prompt = prompt.replace("{{EXISTING_CODE}}", code)
-        prompt = prompt.replace("{{ADDITIONAL_FILE}}", additional_file)
+        prompt = prompt.replace("{{ADDITIONAL_FILE}}", additional_file[0].getvalue().decode("utf-8"))
         response = call_claude_sonnet_3_5(system, prompt, assistant)
         st.markdown(response)
